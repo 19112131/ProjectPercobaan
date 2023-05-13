fb9c69c56fd27f47d747b7da6be2615d2bb35361
Salma Niftrina <91085097+19112131@users.noreply.github.com>
2023-05-13 17:50:34 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    
+    <?php
+      $angka = 3;
+      $hasil = $angka + $angka-1;
+      echo "Penjumlahan angka: $angka ". "<br>";
+      print $angka . " + ". $angka-1 . " = ". $hasil;
+    ?>
+    
+  </body>
+</html>
